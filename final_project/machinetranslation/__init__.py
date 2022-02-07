d7b4cf83ac916b4293986be4d03befbff5e9cf0f
nataliexshen <nataliexshen@gmail.com>
2022-02-07 00:36:57 +0000

    Final changes

---
+++ final_project/machinetranslation/__init__.py
@@ -0,0 +1,3 @@
+from . import machinetranslation
+from . import tests
+from . import translator
--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,17 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
+    frenchText= translator.englishToFrench(textToTranslate)
     return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
+    englishText= translator.frenchToEnglish(textToTranslate)
     return "Translated text to English"
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
